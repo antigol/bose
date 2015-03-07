151f7fdd4495ec26073e23729cdbb5e1f8af4452
Mario Geiger <geiger.mario@gmail.com>
2015-03-07 17:41:46 +0100

    report

--- bose.m
+++ bose.m
@@ -1,6 +1,6 @@
 function U = bose(TAU, N)
 
-alpha = 2 / (sqrt(pi) * zeta(3/2));
+c = 2 / (sqrt(pi) * zeta(3/2));
 
 function z = f(x, u, tau) 
 	z = x.^0.5 ./ (exp((x - u) / tau) - 1);
@@ -11,12 +11,14 @@ U = zeros(size(TAU));
 for i=1:length(TAU),
 	tau = TAU(i);
 	
-	[U(i), ignore, info] = fminbnd(@(u) abs(1 - 1/N / (exp(-u/tau)-1) - alpha * integral(@(x) f(x, u, tau), 0, Inf)), -100, 0, optimset('TolX',1e-5));
+	[U(i), ignore, info] = ...
+		fminbnd(@(u) ...
+			abs(1 - 1/N / (exp(-u/tau)-1) - c * integral(@(x) f(x, u, tau), 0, Inf)), ...
+		-10, 0, optimset('TolX',1e-5));
 	
 	if info~=1,
 		display('error');
 	end
 end
-
 
 end
--- ex.m
+++ ex.m
@@ -1,6 +1,10 @@
+clear all;
+close all;
+clc;
+
 tau = linspace(1e-2, 3, 100);
 
-N = [1, 2, 5, 10, 1e4];
+N = [1, 2, 5, 10, 1e4, 1e10];
 
 u = zeros(length(N), length(tau));
 
@@ -9,11 +13,14 @@ for k = 1:length(N),
 end
 
 plot(tau, u);
-
+a = axis();
+hold on;
+plot(tau, -zeta(3/2)^2/(4*pi) * tau.^-2 .* (tau.^(3/2)-1).^2, '--k');
+axis(a);
 %legendCell = cellstr(num2str(N, 'N = %-d'));
 %legend(legendCell);
 
-legend('N=1', 'N=2', 'N=5', 'N=10', 'N=1e4')
+legend('N=1', 'N=2', 'N=5', 'N=10', 'N=1e4', 'N=1e10', 'Approximation theorique');
 
 xlabel('temperature')
 ylabel('potentiel chimique');
