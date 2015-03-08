82f6bd6e57bc0e133ddbcc4f7bd9d0617d099bf1
Mario Geiger <geiger.mario@gmail.com>
2015-03-08 19:23:31 +0100

    ...

--- bose.m
+++ bose.m
@@ -12,17 +12,17 @@ Nrel = zeros(size(TAU));
 for i=1:length(TAU),
 	tau = TAU(i);
 	
-	[U(i), ignore, info] = ...
-		fminbnd(@(u) ...
-			abs(1 - 1/N / (exp(-u/tau)-1) - c * integral(@(x) f(x, u, tau), 0, Inf)), ...
-		-10, 0, optimset('TolX',1e-5));
+	[U(i), ignore, info] = fminbnd(@(u) ...
+		abs(1 - 1/N / (exp(-u/tau)-1) - c * integral(@(x) f(x, u, tau) ...
+			, 0, Inf)), -10, 0, optimset('TolX',1e-5));
 	
 	if info~=1,
 		display('error');
 	end
 	
-	dN2 = 1/(2*cosh(U(i)/tau)-2) + 2*N/(sqrt(pi) * zeta(3/2)) * integral(@(x) sqrt(x)./(2*cosh((x-U(i))/tau)-2), 0, Inf);
+	dN2 = 1/(2*cosh(U(i)/tau)-2) + 2*N/(sqrt(pi) * zeta(3/2)) * ...
+		integral(@(x) sqrt(x)./(2*cosh((x-U(i))/tau)-2), 0, Inf);
+	
 	Nrel(i) = sqrt(dN2) / N;
 end
-
 end
